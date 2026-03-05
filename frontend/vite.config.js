dd8673d4e12ace632385bfce1cf1027795535cc9
Lukas Schrenk <lschrenk@student.tgm.ac.at>
2026-03-05 08:09:10 +0100

    Login geändert

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  envDir: path.resolve(__dirname, '..'), // Lade .env aus dem übergeordneten Verzeichnis
   plugins: [
     vue(),
     vueDevTools(),
